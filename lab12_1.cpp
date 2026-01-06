d4612e0c2b9d739002cb6c3c36a5ec4548e01839
dpassawit <nitee.dome@gmail.com>
2026-01-06 15:18:28 +0700

    lab12 complete

--- lab12_1.cpp
+++ lab12_1.cpp
@@ -1,17 +1,22 @@
 #include<iostream>
 using namespace std;
 
-int fibonacci(int);
+long long int fibonacci(long long int x);
 
 int main(){
-    int x;
-    cin >> x;
-    cout << fibonacci(x);
-    return 0; 
+    clock_t start = clock();
+    cout << "Result: " << fibonacci(50) << "\n";
+    clock_t end = clock();
+    double elapsed = double(end - start)/CLOCKS_PER_SEC;
+    cout << "Elapsed Time: " << elapsed << " seconds.";
+    return 0;
 }
 
-int fibonacci(int n){
-    if(n <= 1)
-        return n;
-    return fibonacci(n-1) + fibonacci(n-2);
+
+long long int fibonacci(long long int x){
+    if(x <= 1) return x;
+    else{
+        x = fibonacci(x-1) + fibonacci(x-2);
+        return x;
+    }
 }
--- lab12_4.cpp
+++ lab12_4.cpp
@@ -2,6 +2,9 @@
 
 using namespace std;
 
+template <typename currentType>
+void mySwap(currentType &,currentType &);
+
 int main(){
 	int x, y;
 	string a, b;
@@ -30,3 +33,9 @@ int main(){
 	return 0;
 }
 
+template <typename currentType>
+void mySwap(currentType &x,currentType &y){
+	currentType z = x;
+	x = y;
+	y = z;
+}
